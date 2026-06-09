4565b7eaffc9fa33669aed072e288ce1fbd82b42
Irenacr <Irenacahyaresinah@gmail.com>
2026-06-10 00:13:29 +0700

    fix transaksis migration

--- database/migrations/2026_06_06_190348_modify_transaksi_table.php
+++ database/migrations/2026_06_06_190348_modify_transaksi_table.php
@@ -8,27 +8,47 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::table('transaksis', function (Blueprint $table) {
+        if (Schema::hasTable('transaksis')) {
 
-            $table->dropColumn([
-                'barang_id',
-                'jumlah',
-                'kategori'
-            ]);
+            Schema::table('transaksis', function (Blueprint $table) {
 
-        });
+                if (Schema::hasColumn('transaksis', 'barang_id')) {
+                    $table->dropColumn('barang_id');
+                }
+
+                if (Schema::hasColumn('transaksis', 'jumlah')) {
+                    $table->dropColumn('jumlah');
+                }
+
+                if (Schema::hasColumn('transaksis', 'kategori')) {
+                    $table->dropColumn('kategori');
+                }
+
+            });
+
+        }
     }
 
     public function down(): void
     {
-        Schema::table('transaksis', function (Blueprint $table) {
+        if (Schema::hasTable('transaksis')) {
 
-            $table->unsignedBigInteger('barang_id');
+            Schema::table('transaksis', function (Blueprint $table) {
 
-            $table->integer('jumlah')->default(1);
+                if (!Schema::hasColumn('transaksis', 'barang_id')) {
+                    $table->unsignedBigInteger('barang_id')->nullable();
+                }
 
-            $table->string('kategori')->nullable();
+                if (!Schema::hasColumn('transaksis', 'jumlah')) {
+                    $table->integer('jumlah')->default(1);
+                }
 
-        });
+                if (!Schema::hasColumn('transaksis', 'kategori')) {
+                    $table->string('kategori')->nullable();
+                }
+
+            });
+
+        }
     }
 };
